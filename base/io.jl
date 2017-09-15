4237a46af924561c2e94f37278cfacf48e126fea
Jameson Nash <vtjnash@gmail.com>
2017-09-15 12:50:59 -0400

    [wip] reduce code duplication, allow non-Int indexes

--- base/io.jl
+++ base/io.jl
@@ -508,51 +508,73 @@ function readuntil(s::IO, delim::T) where T
     return out
 end
 
-mutable struct Backtrack{T, C<:AbstractArray}
-    target::Vector{T}
+mutable struct Backtrack{T, S<:AbstractVector, O, C}
+    target::T
+    states::S
+    out::O
+    cache::C
     pos::Int
     len::Int
-    cache::C
     max_pos::Int
 end
 
-function Backtrack(target::AbstractString)
-    t = collect(target)
-    len = length(t)
+Backtrack(target, len::Int, states, out) =
+    Backtrack(target, states, out, spzeros(Int, len), 1, len, 1)
+
+function Backtrack(target::AbstractString, state1)
+    len = length(target)
     # Setting max_pos to 1 means that backtrack[1] == 0
-    Backtrack(t, 0, len, spzeros(Int, len), 1)
+    states = Vector{typeof(state1)}(len)
+    states[1] = state1
+    return Backtrack(target, len, states, IOBuffer())
 end
 
-function Backtrack(target::String)
-    t = Vector{UInt8}(target)  # convert String to a utf8-byte-iterator
-    len = length(t)
-    Backtrack(t, 0, len, spzeros(Int, len), 1)
-end
+#function Backtrack(target::String, state1::Int)
+#    t = Vector{UInt8}(target)  # convert String to a utf8-byte-iterator
+#    len = length(t)
+#    return Backtrack(t, len, OneTo(len), StringVector(0))
+#end
 
-function backtrack(bt::Backtrack, index::Integer)
-    for i = (bt.max_pos + 1):index
+function prep_backtrack_cache(bt::Backtrack, index::Int)
+    # grow cache up to index
+    for i in (bt.max_pos + 1):index
         b = bt.cache[i - 1] + 1
         # Requires that i > b. We can avoid this check since we start max_pos at 1.
-        if bt.target[i] == bt.target[b]
+        ci = next(bt.target, bt.states[i])[1]
+        cb = next(bt.target, bt.states[b])[1]
+        if ci == cb
             bt.cache[i] = b
         end
     end
     bt.max_pos = index
-    return bt.cache[index]
+    nothing
 end
 
-@inline function record{T}(bt::Backtrack{T}, c::T)
-    i = bt.pos
+@inline function record(bt::Backtrack, c)
     # Backtrack until the next target character matches what was found
-    while i > 0 && c != bt.target[i + 1]
-        i = backtrack(bt, i)
+    if bt.out isa IO
+        write(bt.out, c)
+    else
+        push!(bt.out, c)
     end
-    if c == bt.target[i + 1]
-        i += 1
+    pos = bt.pos
+    while true
+        c1, pos1 = next(bt.target, bt.states[pos])
+        if c == c1
+            pos += 1
+            isa(bt.states, Vector) && (bt.states[pos] = pos1)
+            break
+        end
+        pos == 1 && break
+        pos -= 1
+        prep_backtrack_cache(bt, pos)
+        pos = bt.cache[pos]
     end
-    bt.pos = i
-    return i == bt.len
+    bt.pos = pos
+    return pos == bt.len
 end
+
+take!(bt::Backtrack) = isa(bt.out, IO) ? take!(bt.out) : bt.out
 
 function readuntil(io::IO, target::AbstractString)
     i = start(target)
@@ -561,35 +583,14 @@ function readuntil(io::IO, target::AbstractString)
     end
     c, i = next(target, i)
     if done(target, i) && c < Char(0x80)
-        return readuntil_string(s, c % UInt8)
+        return readuntil_string(io, c % UInt8)
     end
-    backtrack = Backtrack(target)
-    out = IOBuffer()
+    backtrack = Backtrack(target, i)
     while !eof(io)
         c = read(io, Char)
-        write(out, c)
         record(backtrack, c) && break
     end
-    return String(take!(out))
-end
-
-function readuntil(s::IO, target::String)
-    i = start(target)
-    if done(target, i)
-        return ""
-    end
-    c, i = next(target, i)
-    if done(target, i) && c < Char(0x80)
-        return readuntil_string(s, c % UInt8)
-    end
-    backtrack = Backtrack(target)
-    out = Base.StringVector(0)
-    while !eof(s)
-        byte = read(s, UInt8)
-        push!(out, byte)
-        record(backtrack, byte) && break
-    end
-    return String(out)
+    return String(take!(backtrack))
 end
 
 """
